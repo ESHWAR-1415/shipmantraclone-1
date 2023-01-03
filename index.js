e48a1f10d91a7df61dae19b943bebba492c66dc9
e19cse371 <e19cse371@bennett.edu.in>
2023-01-03 17:16:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+for(let i=0;i<document.querySelectorAll(".bt").length;i++){
+
+document.querySelectorAll(".bt")[i].addEventListener("click",myfun);
+}
+
+function myfun(){
+    alert("hi bro")
+}*/
+
